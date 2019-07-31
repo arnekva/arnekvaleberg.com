30ec852b77856e4754250b8246212399a8624a5b
Arne Kvaleberg <arnekva@gmail.com>
2019-07-31 09:48:14 +0200

    Fixed exception when mobile browser scrolls too far up

--- js/titleupdate.js
+++ js/titleupdate.js
@@ -8,7 +8,19 @@ setInterval( function() {
   if ( scrolling ) {
     scrolling = false;
     var el = document.getElementsByClassName('nav-link js-scroll-trigger active');
-    document.title = el[0].innerHTML + ' | Arne Kvaleberg';
-    console.log('Title oppdatert med innhold \'' + el[0].innerHTML + '\'');
+  //Kaster exception hvis brukeren scroller over toppen (bounce) på mobil, da ingen seksjon vil være active fordi han forlater vinduet. Må sjekke om tabell er tom.
+    if(el.length > 0){
+      document.title = el[0].innerHTML + ' | Arne Kvaleberg';
+      console.log('Title oppdatert med innhold \'' + el[0].innerHTML + '\'');
+
+    } else{
+      document.title = 'Om | Arne Kvaleberg';
+      console.log('Scrolled too far up, lost active class. Set to default.');
+    }
+
+
+
+
+
   }
 }, 800 );
