17833812e0f623819f326b693fa6ad2c23e28582
Arne Kvaleberg <arnekva@gmail.com>
2019-08-06 13:47:25 +0200

    Changed scripts for score
    Changed into Jquery to more efficiently get current elements to update scores

---
+++ oldscore.js
@@ -0,0 +1,98 @@
+function updateScoreP1()
+{
+  var sum1 = 0;
+
+  sum1 += parseInt(document.getElementById("3x3p1").value);
+  sum1 += parseInt(document.getElementById("3x4p1").value);
+  sum1 += parseInt(document.getElementById("4x4p1").value);
+  sum1 += parseInt(document.getElementById("3x3x3p1").value);
+  sum1 += parseInt(document.getElementById("3x3x4p1").value);
+  sum1 += parseInt(document.getElementById("3x4x4p1").value);
+  sum1 += parseInt(document.getElementById("4x4x4p1").value);
+  sum1 += parseInt(document.getElementById("3x3x3x3p1").value);
+    document.getElementById("numbVal1").innerHTML = sum1;
+}
+function updateScoreP2()
+{
+  var sum2 = 0;
+
+  sum2 += parseInt(document.getElementById("3x3p2").value);
+  sum2 += parseInt(document.getElementById("3x4p2").value);
+  sum2 += parseInt(document.getElementById("4x4p2").value);
+  sum2 += parseInt(document.getElementById("3x3x3p2").value);
+  sum2 += parseInt(document.getElementById("3x3x4p2").value);
+  sum2 += parseInt(document.getElementById("3x4x4p2").value);
+  sum2 += parseInt(document.getElementById("4x4x4p2").value);
+  sum2 += parseInt(document.getElementById("3x3x3x3p2").value);
+    document.getElementById("numbVal2").innerHTML = sum2;
+}
+function updateScoreP3()
+{
+  var sum3 = 0;
+
+  sum3 += parseInt(document.getElementById("3x3p3").value);
+  sum3 += parseInt(document.getElementById("3x4p3").value);
+  sum3 += parseInt(document.getElementById("4x4p3").value);
+  sum3 += parseInt(document.getElementById("3x3x3p3").value);
+  sum3 += parseInt(document.getElementById("3x3x4p3").value);
+  sum3 += parseInt(document.getElementById("3x4x4p3").value);
+  sum3 += parseInt(document.getElementById("4x4x4p3").value);
+  sum3 += parseInt(document.getElementById("3x3x3x3p3").value);
+    document.getElementById("numbVal3").innerHTML = sum3;
+}
+function updateScoreP4()
+{
+  var sum4 = 0;
+
+  sum4 += parseInt(document.getElementById("3x3p4").value);
+  sum4 += parseInt(document.getElementById("3x4p4").value);
+  sum4 += parseInt(document.getElementById("4x4p4").value);
+  sum4 += parseInt(document.getElementById("3x3x3p4").value);
+  sum4 += parseInt(document.getElementById("3x3x4p4").value);
+  sum4 += parseInt(document.getElementById("3x4x4p4").value);
+  sum4 += parseInt(document.getElementById("4x4x4p4").value);
+  sum4 += parseInt(document.getElementById("3x3x3x3p4").value);
+    document.getElementById("numbVal4").innerHTML = sum4;
+}
+function updateScoreP5()
+{
+  var sum5 = 0;
+
+  sum5 += parseInt(document.getElementById("3x3p5").value);
+  sum5 += parseInt(document.getElementById("3x4p5").value);
+  sum5 += parseInt(document.getElementById("4x4p5").value);
+  sum5 += parseInt(document.getElementById("3x3x3p5").value);
+  sum5 += parseInt(document.getElementById("3x3x4p5").value);
+  sum5 += parseInt(document.getElementById("3x4x4p5").value);
+  sum5 += parseInt(document.getElementById("4x4x4p5").value);
+  sum5 += parseInt(document.getElementById("3x3x3x3p5").value);
+    document.getElementById("numbVal5").innerHTML = sum5;
+}
+function updateScoreP6()
+{
+  var sum6 = 0;
+
+  sum6 += parseInt(document.getElementById("3x3p6").value);
+  sum6 += parseInt(document.getElementById("3x4p6").value);
+  sum6 += parseInt(document.getElementById("4x4p6").value);
+  sum6 += parseInt(document.getElementById("3x3x3p6").value);
+  sum6 += parseInt(document.getElementById("3x3x4p6").value);
+  sum6 += parseInt(document.getElementById("3x4x4p6").value);
+  sum6 += parseInt(document.getElementById("4x4x4p6").value);
+  sum6 += parseInt(document.getElementById("3x3x3x3p6").value);
+    document.getElementById("numbVal6").innerHTML = sum6;
+}
+function updateScoreP7()
+{
+  var sum7 = 0;
+
+  sum7 += parseInt(document.getElementById("3x3p7").value);
+  sum7 += parseInt(document.getElementById("3x4p7").value);
+  sum7 += parseInt(document.getElementById("4x4p7").value);
+  sum7 += parseInt(document.getElementById("3x3x3p7").value);
+  sum7 += parseInt(document.getElementById("3x3x4p7").value);
+  sum7 += parseInt(document.getElementById("3x4x4p7").value);
+  sum7 += parseInt(document.getElementById("4x4x4p7").value);
+  sum7 += parseInt(document.getElementById("3x3x3x3p7").value);
+    document.getElementById("numbVal7").innerHTML = sum7;
+}
